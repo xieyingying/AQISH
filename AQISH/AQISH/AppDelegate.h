af5f55e3a9bcc438b7c6891da0e37740ac958a2b
xiebohui <bohui.xie@dianping.com>
2015-08-08 19:57:34 +0800

    更改工程

---
+++ AQISH/AQISH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AQISH
+//
+//  Created by xiebohui on 8/8/15.
+//  Copyright (c) 2015 xiebohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AQISH/AQISH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AQISH
+//
+//  Created by xiebohui on 8/8/15.
+//  Copyright (c) 2015 xiebohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
