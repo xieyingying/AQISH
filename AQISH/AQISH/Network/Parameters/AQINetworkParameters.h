351e5ec7f3768c59f17de380f486021d7cef34ae
xiebohui <bohui.xie@dianping.com>
2015-08-12 10:26:30 +0800

    Add AQINetworkParameters

---
+++ AQISH/AQISH/Network/Parameters/AQINetworkParameters.h
@@ -0,0 +1,13 @@
+//
+//  AQINetworkParameters.h
+//  AQISH
+//
+//  Created by xiebohui on 8/12/15.
+//  Copyright (c) 2015 xiebohui. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface AQINetworkParameters : MTLModel
+
+@end
---
+++ AQISH/AQISH/Network/Parameters/AQINetworkParameters.m
@@ -0,0 +1,13 @@
+//
+//  AQINetworkParameters.m
+//  AQISH
+//
+//  Created by xiebohui on 8/12/15.
+//  Copyright (c) 2015 xiebohui. All rights reserved.
+//
+
+#import "AQINetworkParameters.h"
+
+@implementation AQINetworkParameters
+
+@end
