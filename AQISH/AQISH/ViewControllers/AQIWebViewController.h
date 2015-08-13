bb53ac06f99357b6c8e54a5910a7b0d6c4bd4d89
xiebohui <bohui.xie@dianping.com>
2015-08-13 13:00:17 +0800

    Add AQIWebViewController

---
+++ AQISH/AQISH/ViewControllers/AQIWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  AQIWebViewController.h
+//  AQISH
+//
+//  Created by xiebohui on 8/13/15.
+//  Copyright (c) 2015 xiebohui. All rights reserved.
+//
+
+#import "AQIBaseViewController.h"
+
+@interface AQIWebViewController : AQIBaseViewController
+
+@end
---
+++ AQISH/AQISH/ViewControllers/AQIWebViewController.m
@@ -0,0 +1,25 @@
+//
+//  AQIWebViewController.m
+//  AQISH
+//
+//  Created by xiebohui on 8/13/15.
+//  Copyright (c) 2015 xiebohui. All rights reserved.
+//
+
+#import "AQIWebViewController.h"
+
+@interface AQIWebViewController ()
+
+@end
+
+@implementation AQIWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)commonInit {
+    
+}
+
+@end
